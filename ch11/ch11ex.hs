4069ea12b1bd37b4a79316aff993eb450edced65
Matt Olson <matt.j.olson@gmail.com>
2017-12-10 14:34:18 -0800

    Infix operator names, some stuff on HKTs, binary tree code

--- ch11/ch11ex.hs
+++ ch11/ch11ex.hs
@@ -69,3 +69,48 @@ withAge i k = k i
 
 isGood :: Bool -> (Bool -> Cat) ->  Cat
 isGood g k = k g
+
+-- 11.17, binary trees
+data BinaryTree a =
+      Leaf
+    | Node (BinaryTree a) a (BinaryTree a)
+    deriving (Eq, Show)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert' b Leaf = Node Leaf b Leaf
+insert' b (Node left a right)
+    | b == a = Node left a right
+    | b <  a = Node (insert' b left) a right
+    | a <  b = Node left a (insert' b right)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree f Leaf = Leaf
+mapTree f (Node left a right) = 
+    Node (mapTree f left) (f a) (mapTree f right)
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) =
+    a : preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) =
+    inorder left ++ [a] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) =
+    postorder left ++ postorder right ++ [a]
+
+-- postorder, we'll fold left, then right, then this
+cataTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+cataTree f z Leaf = z
+cataTree f z (Node left a right) =
+    f a righted where
+    righted = cataTree f lefted right
+    lefted  = cataTree f z left
+
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+-- 11.18, chapter exercises
